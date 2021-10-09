a9e610fe0fdd23123bff43e20427a05edf5404a0
doublescoop <ec897@cornell.edu>
2021-10-09 00:50:59 -0400

    part2 done

--- Lab 3/demo/get_device.py
+++ Lab 3/demo/get_device.py
@@ -3,4 +3,4 @@ import pyaudio
 audio = pyaudio.PyAudio()
 
 for ii in range(audio.get_device_count()):
-    print(ii, audio.get_device_info_by_index(ii).get('name'))
+    print(ii, audio.get_device_info_by_index(ii).get("name"))
--- Lab 3/playwithme.py
+++ Lab 3/playwithme.py
@@ -69,27 +69,28 @@ buttonB = digitalio.DigitalInOut(board.D24)
 buttonA.switch_to_input()
 buttonB.switch_to_input()
 
-# Initialize Green LED to visually signal players 
+# Initialize Green LED to visually signal players
 # when they're correct
 buttonG = qwiic_button.QwiicButton()
 
 
-# Initialize accelerometer 
+# Initialize accelerometer
 # i2c = busio.I2C(board.SCL, board.SDA)
 # mpu = adafruit_mpu6050.MPU6050(i2c)
 # address = 0x68
 
 # Initialize proximity sensor to initiate the interaction
-i2c = board.I2C()   # uses board.SCL and board.SDA
+i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = APDS9960(i2c)
 sensor.enable_proximity = True
 
 
 def speak(val):
-    subprocess.run(["sh","GoogleTTS_demo.sh",val])
+    subprocess.run(["sh", "GoogleTTS_demo.sh", val])
+
 
 def check_userinput():
-    os.system('arecord -D hw:2,0 -f cd -c1 -r 48000 -d 4 -t wav recorded_mono.wav')
+    os.system("arecord -D hw:2,0 -f cd -c1 -r 48000 -d 4 -t wav recorded_mono.wav")
     wf = wave.open("recorded_mono.wav", "rb")
 
     model = Model("model")
@@ -104,17 +105,18 @@ def check_userinput():
         rec.AcceptWaveform(data)
 
     reply = json.loads(rec.FinalResult())
-    print("User\'s reply: ", reply["text"])
+    print("User's reply: ", reply["text"])
     return reply["text"]
+
 
 # def stopwatch(seconds):
 #     start = time.time()
-#     time.clock()    
+#     time.clock()
 #     elapsed = 0
 #     while elapsed < seconds:
 #         elapsed = time.time() - start
-#         print ("loop cycle time: %f, seconds count: %02d % (time.clock() , elapsed)") 
-#         time.sleep(1)  
+#         print ("loop cycle time: %f, seconds count: %02d % (time.clock() , elapsed)")
+#         time.sleep(1)
 #     return elapsed
 
 
@@ -124,14 +126,14 @@ while True:
     prox = sensor.proximity
     if prox > 10:
         main_image = Image.open("images/a_pickmeup.png")
-        main_image = main_image.convert('RGB')
+        main_image = main_image.convert("RGB")
         main_image = main_image.resize((width, height), Image.BICUBIC)
         disp.image(main_image, rotation)
         speak("Hi, pick me up!")
         time.sleep(2)
         break
 
-#begin interaction when picked up
+# begin interaction when picked up
 # while True:
 #     gyro = mpu.gyro
 #     if gyro == 0.0:
@@ -141,138 +143,145 @@ while True:
 
 while True:
     main_image = Image.open("images/a_intro.png")
-    main_image = main_image.convert('RGB')
+    main_image = main_image.convert("RGB")
     main_image = main_image.resize((width, height), Image.BICUBIC)
     disp.image(main_image, rotation)
-    #ask name and test microphone
+    # ask name and test microphone
     speak("Hi, Thank you for picking me up. What is your name?")
     reply_name = check_userinput()
     if len(reply_name) > 1:
         speak(str(reply_name))
         speak("What is your hobby?")
     else:
-        speak("Please speak louder to the microphon on the side, what is your name again?")
+        speak(
+            "Please speak louder to the microphon on the side, what is your name again?"
+        )
         reply_name = check_userinput()
         speak(str(reply_name))
         speak("What is your hobby?")
 
-    #ask hobby and initiate the game
+    # ask hobby and initiate the game
     reply_hobby = check_userinput()
     if len(reply_hobby) > 1:
         speak(str(reply_hobby))
-        speak("That's interesting. I like to draw. Do you want to see my drawings and guess what they are?")
-    else: 
-        speak("I didn't hear you well. anyhow, my hobby is drawing. Do you want to see my drawings and guess what they are?")
+        speak(
+            "That's interesting. I like to draw. Do you want to see my drawings and guess what they are?"
+        )
+    else:
+        speak(
+            "I didn't hear you well. anyhow, my hobby is drawing. Do you want to see my drawings and guess what they are?"
+        )
 
     reply_start = check_userinput()
-    if("yes" or "sure" or "okay" or "good" or "cool" in reply_start):
-        speak("cool! I have six pics. Guess in three seconds. Say only once, closer to the microphone on the side.")
+    if "yes" or "sure" or "okay" or "good" or "cool" in reply_start:
+        speak(
+            "cool! I have six pics. Guess in three seconds. Say only once, closer to the microphone on the side."
+        )
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
         q1 = Image.open("images/a_intro.png")
-        q1 = q1.convert('RGB')
+        q1 = q1.convert("RGB")
         q1 = q1.resize((width, height), Image.BICUBIC)
         disp.image(q1, rotation)
         time.sleep(2)
         # Draw a black filled box to clear the image.
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
         q1 = Image.open("images/01_turtle.png")
-        q1 = q1.convert('RGB')
+        q1 = q1.convert("RGB")
         q1 = q1.resize((width, height), Image.BICUBIC)
         disp.image(q1, rotation)
         time.sleep(1)
         reply = check_userinput()
-        if("turtle" in reply or "tortoise" in reply):
+        if "turtle" in reply or "tortoise" in reply:
             score += 1
             buttonG.LED_on(150)
             speak("correct")
             buttonG.LED_off()
-        else: 
+        else:
             speak("wrong")
 
         speak("next")
         q2 = Image.open("images/02_icecream.png")
-        q2 = q2.convert('RGB')
+        q2 = q2.convert("RGB")
         q2 = q2.resize((width, height), Image.BICUBIC)
         disp.image(q2, rotation)
         time.sleep(1)
         reply = check_userinput()
-        if("icecream" or "ice cream" or "i scream" in reply):
+        if "icecream" or "ice cream" or "i scream" in reply:
             score += 1
             buttonG.LED_on(150)
             speak("correct")
             buttonG.LED_off()
-        else: 
+        else:
             speak("wrong")
 
-
         speak("next")
-        q3 = Image.open("images/03_skull.png").convert('RGB')
+        q3 = Image.open("images/03_skull.png").convert("RGB")
         q3 = q3.resize((width, height), Image.BICUBIC)
         disp.image(q3, rotation)
         time.sleep(1)
         reply = check_userinput()
-        if("skull" in reply):
+        if "skull" in reply:
             score += 1
             buttonG.LED_on(150)
             speak("correct")
             buttonG.LED_off()
-        else: 
-            speak("wrong")
-
+        else:
+            speak("wrong, it's a skull")
 
         speak("next")
-        q4 = Image.open("images/05_yoga.png").convert('RGB')
+        q4 = Image.open("images/05_yoga.png").convert("RGB")
         q4 = q4.resize((width, height), Image.BICUBIC)
         disp.image(q4, rotation)
         time.sleep(1)
         reply = check_userinput()
-        if("yoga" in reply):
+        if "yoga" in reply:
             score += 1
             buttonG.LED_on(150)
             speak("correct")
             buttonG.LED_off()
-        else: 
-            speak("wrong")
-
+        else:
+            speak("wrong, I drew yoga")
 
         speak("next")
-        q5 = Image.open("images/06_beard.png").convert('RGB')
+        q5 = Image.open("images/09_hotdog.png").convert("RGB")
         q5 = q5.resize((width, height), Image.BICUBIC)
         disp.image(q5, rotation)
         time.sleep(1)
         reply = check_userinput()
-        if("beard" in reply):
+        if "hotdog" in reply:
             score += 1
             buttonG.LED_on(150)
             speak("correct")
             buttonG.LED_off()
-        else: 
-            speak("wrong")
+        else:
+            speak("wrong, it's hotdog")
 
         speak("next")
-        q6 = Image.open("images/10_asparagus.png").convert('RGB')
+        q6 = Image.open("images/10_asparagus.png").convert("RGB")
         q6 = q6.resize((width, height), Image.BICUBIC)
         disp.image(q6, rotation)
         time.sleep(1)
         reply = check_userinput()
-        if("asparagus" in reply):
+        if "asparagus" in reply:
             score += 1
             buttonG.LED_on(150)
             speak("correct")
             buttonG.LED_off()
-        else: 
-            speak("wrong")
+        else:
+            speak("wrong, it's asparagus")
 
         speak("your score is" + str(score))
-        if score < 3: speak("you don't get my drawings")
-        elif score >=3 and score < 6: speak("You are pretty good")
-        elif score == 6 : speak("oh my god you must be my soulmate")
+        if score < 3:
+            speak("you don't get my drawings")
+        elif score >= 3 and score < 6:
+            speak("You are pretty good")
+        elif score == 6:
+            speak("oh my god you must be my soulmate")
 
-    else: 
+    else:
         speak("I assume you don't want to play with me.")
         time.sleep(1)
         break
-
 
     speak("bye, let's play next time")
     time.sleep(0.5)
